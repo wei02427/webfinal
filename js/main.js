77e1a7d52b9b7b78b8e7c0d370dd40c4e4520d2b
wei02427 <47982592+wei02427@users.noreply.github.com>
2019-06-20 17:54:04 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -539,7 +539,7 @@ $("#clearproject").click(function() {
     var name;
     $("#messageInput").keypress(function(e) {
       console.log("press");
-      if (e.keyCode == 13) {
+      if (e.keyCode == 13 && $messageField.val()!='') {
         loginUser = firebase.auth().currentUser;
         firebase
           .database()
