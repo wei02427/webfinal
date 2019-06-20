521fd756862f92e82382804e71d58e3909fc289d
wei02427 <47982592+wei02427@users.noreply.github.com>
2019-06-20 13:52:22 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -454,7 +454,46 @@ $(document).ready(function() {
       .ref("/COMMENT")
       .remove();
   });
-
+ $("#modifyproject").click(function() {
+   alert("已儲存");
+  var nameElement = document.getElementById("projectName");
+  var name = nameElement.value;
+  var arefElement = document.getElementById("arefVideo");
+  var aref = arefElement.value;
+  var classElement = document.getElementById("class");
+  var cls = classElement.value;
+  var commentElement = document.getElementById("comment");
+  var comment = commentElement.value;
+  var gitElement = document.getElementById("gitaref");
+  var git = gitElement.value;
+  firebase
+    .database()
+    .ref("users/" + firebase.auth().currentUser.uid + "/projectgoal")
+    .set({
+      projectName: name,
+      arefVideo: aref,
+      class: cls,
+      comment: comment,
+      git: git
+    });
+  });
+$("#clearproject").click(function() {
+ alert("已清除");
+  var nameElement = document.getElementById("projectName");
+  nameElement.value = "";
+  var arefElement = document.getElementById("arefVideo");
+  arefElement.value = "";
+  var classElement = document.getElementById("class");
+  classElement.value = "";
+  var commentElement = document.getElementById("comment");
+  commentElement.value = "";
+  var gitElement = document.getElementById("gitaref");
+  gitElement.value = "";
+  firebase
+    .database()
+    .ref("users/" + firebase.auth().currentUser.uid + "/projectgoal")
+    .remove();
+  });
   function checkID() {
     const adminUID = "ckhZgIhh7RUPKWaRtSx4k3okia02";
     firebase.auth().onAuthStateChanged(function(user) {
