800b34e538753a9009781db1256d768d3903c984
wei02427 <47982592+wei02427@users.noreply.github.com>
2019-06-20 12:59:26 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -209,7 +209,7 @@ $(document).ready(function() {
   //----------------如果網頁第一次要載入值 寫在這裡----------------------
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      if (location.pathname == "/html/progress.html") refreshProgress();
+      if (location.pathname == "/webfinal/html/progress.html") refreshProgress();
       console.log("test");
       getId();
       getPhoto();
@@ -267,8 +267,8 @@ $(document).ready(function() {
       .then(function(url) {
         document.getElementById("photo").style.cssText =
           "background-image:url( " + url + ");";
-      alert(location.pathname);
-        if (location.pathname == "/html/setting.html")
+      //alert(location.pathname);
+        if (location.pathname == "/webfinal/html/setting.html")
           document.getElementById("upload_img").style.cssText =
             "background-image:url( " + url + ");";
       })
